9f460a78bd7f900e7bc038b0d7dce5c2697ba611
Keith Lee <lee.mingkeith@gmail.com>
2020-04-24 00:50:19 +0900

    Initial Commit

---
+++ HeartLight/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HeartLight
+//
+//  Created by Keith Lee on 2020/04/24.
+//  Copyright © 2020 Keith Lee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
